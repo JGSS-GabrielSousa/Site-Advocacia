2480ed3d3cb5f0547a49a13fb1b30e7df826fb40
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2023-08-31 16:17:03 -0300

    botao whatsapp

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-document.querySelector("#contact").addEventListener("click", async e => {
+async function whatsappChat(){
     const { value: msg } = await Swal.fire({
         title: "Digite sua mensagem",
         inputLabel: "Logo entraremos em contato para lhe atender",
         input: "text",
         inputValue: "Olá, estou com dúvidas",
         showCancelButton: true,
+        cancelButtonText: 'Cancelar',
     });
 
     if(msg){
         const url = `https://api.whatsapp.com/send?phone=9900123456789&text=${msg}`;
         window.open(url, '_blank').focus();
     }
-});
+}
+
+document.querySelector("#contact").addEventListener("click", whatsappChat);
+document.querySelector("#whatsapp-contact").addEventListener("click", whatsappChat);

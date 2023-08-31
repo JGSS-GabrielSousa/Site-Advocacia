d742d170001f9c4d5ab7a3520a67a8d306b8e3c0
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2023-08-31 16:15:44 -0300

    fix

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 document.querySelector("#contact").addEventListener("click", async e => {
-
-    
-    //https://api.whatsapp.com/send?phone=5583996032551&text=Ol%C3%A1%20em%20que%20podemos%20ajudar?
-
     const { value: msg } = await Swal.fire({
         title: "Digite sua mensagem",
         inputLabel: "Logo entraremos em contato para lhe atender",
